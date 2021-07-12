106688316b687f6e852051febe84c900ad734070
tta2yta <tta2yta@gmail.com>
2021-07-12 22:56:31 +0300

    Add font awesome file loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: ['file-loader']
+      }
     ],
   },
   devServer: {
