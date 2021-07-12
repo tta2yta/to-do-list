1a8f37d50449e9a1cafe8869c3335051a944049b
tta2yta <tta2yta@gmail.com>
2021-07-12 15:24:10 +0300

    Add displayItems Function

--- src/todolist.js
+++ src/todolist.js
@@ -2,4 +2,18 @@ export default class ToDoList{
     constructor(){
         this.listItems=[{decription:"reading", completed:false, index:1 }]
     }
+
+ displayItems=()=>{
+    const listMain= document.createElement('ul')
+ this.listItems.forEach((item, index)=>{
+     
+     const listItem= document.createElement('li')
+     listItem.id=item.index
+     listItem.className="listItem"
+     listItem.textContent=item.decription
+
+     listMain.appendChild(listItem)
+ })
+ document.getElementById('list-container').appendChild(listMain)
+}
 }
