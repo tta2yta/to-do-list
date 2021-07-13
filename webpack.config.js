54c42a087c851fa461c6dd96fead3c029dcdf167
tta2yta <tta2yta@gmail.com>
2021-07-13 12:52:17 +0300

    Modify package json file

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
     index: './src/index.js',
     todolist: './src/todolist.js',
+    sortingitems: './src/sortingitems.js',
   },
   output: {
     filename: '[name].bundle.js',
