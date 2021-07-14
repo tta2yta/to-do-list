9e73814ca79204a2b18fedca7dae276d76c0cdaf
tta2yta <tta2yta@gmail.com>
2021-07-14 15:43:17 +0300

    Add feature to checkComplete function and add function unCheckComplete

--- src/checkstatus.js
+++ src/checkstatus.js
@@ -1,9 +1,28 @@
 function checkComplete(ev){
-console.log(this.parentNode)
+console.log("jjj")
+
 this.border=0
-this.parentNode.classList.add('completed')
-// this.parentNode.removeChild(this)
-this.parentNode.replaceChild(document.createElement("span"), this);
+this.parentNode.querySelector('.textDesc').classList.add('completed')
+const thickSyb=document.createElement('span');
+thickSyb.className='thickSyb'
+thickSyb.addEventListener('click', unCheckComplete, false);
+const t = document.createTextNode("✔ "); 
+thickSyb.appendChild(t)
+this.parentNode.replaceChild(thickSyb, this);
+
+
+}
+
+function unCheckComplete(e){
+    const checkbox = document.createElement('input');
+     checkbox.type = 'checkbox';
+     checkbox.name = `checkbox-${this.parentNode.id - 1}`;
+     checkbox.value = 'value';
+     checkbox.id = `checkbox-${this.parentNode.id - 1}`;
+     checkbox.className="checkbox"
+     this.parentNode.replaceChild(checkbox, this);
+     checkbox.addEventListener('click', checkComplete, false);
+
 }
 
 function addCheckboxhandler(elem){
