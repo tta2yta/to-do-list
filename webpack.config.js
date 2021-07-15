17467042992d2af4fd47596cf92f02e2c5ee12e9
tta2yta <tta2yta@gmail.com>
2021-07-16 00:40:53 +0300

    Fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     index: './src/index.js',
     todolist: './src/todolist.js',
     sortingitems: './src/sortingitems.js',
-    checkstatus:'./src/checkstatus.js',
+    checkstatus: './src/checkstatus.js',
   },
   output: {
     filename: '[name].bundle.js',
