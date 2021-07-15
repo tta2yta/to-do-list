8b15db56f7c9f7af4e109c87d32c88dbef8d07b8
tta2yta <tta2yta@gmail.com>
2021-07-16 01:48:05 +0300

    Update addItem function

--- src/addremove.js
+++ src/addremove.js
@@ -3,7 +3,13 @@ import ToDoList from "./toDoList";
 export function addItem(ev){
     if(ev.keyCode == 13){
         const listObj= new ToDoList();
-        const description=document.getElementById('listInput').value
+        listObj.getListItmes();
+        const addObj={decription:document.getElementById('listInput').value,completed:false,index:listObj.listItems.length + 1}
+        listObj.listItems=[...listObj.listItems, addObj]
+        listObj.setListItems(listObj.listItems)
+        location.reload();
+        return false;
+
       }
     
 
